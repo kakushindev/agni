322434effebb7c7a9edba934b604f10f2e9d24c8
Ikramullah <ikramullahzx@gmail.com>
2024-02-27 16:23:38 +0800

    feat(app/error): improving onError

--- src/Function/OnError.ts
+++ src/Function/OnError.ts
@@ -1,13 +1,36 @@
 import type { Context, Env } from "hono";
+import type { HTTPException } from "hono/http-exception";
+import type { StatusCode } from "hono/utils/http-status";
 import isJson from "App/Function/IsJson.js";
 
+type ErrorRecord = {
+    message: string;
+    errMessage?: string;
+    uuidErr?: string;
+};
+
 export default function onError(err: Error, c: Context<Env, any, any>): Response {
-    const message = "Something happened, but don't worry maybe it's you or our developer.";
-    const errMessage = err.message;
-    const code = 500;
+    const errResponse: ErrorRecord = {
+        message: ""
+    };
+    errResponse.message = "Something happened, but don't worry maybe it's you or our developer.";
+    errResponse.errMessage = err.message;
+    let code: StatusCode = 500;
+
+    // Expecting this is HTTPError
+    if (Object.keys(err).includes("status")) {
+        const newErr = err as HTTPException;
+        code = newErr.status;
+        errResponse.message = newErr.message;
+        delete errResponse.errMessage;
+    }
 
     const jsonMethod = isJson(c.req.header());
-    if (jsonMethod) return c.json({ message, errMessage }, code);
+    if (jsonMethod) return c.json(errResponse, code);
 
-    return c.text(`${message}\n${errMessage}`, code);
+    // You can improve on this side, this is just example.
+    let msgText = errResponse.message;
+    if (errResponse.errMessage !== undefined) msgText += `\n${errResponse.errMessage}`;
+    if (errResponse.uuidErr !== undefined) msgText += `\nUUID Code: ${errResponse.uuidErr}`;
+    return c.text(msgText, code);
 }
