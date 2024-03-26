57fd2b8b44ab14fc9f7c77faff1cfbb2a734c842
Ikramullah <ikramullahzx@gmail.com>
2024-03-26 23:36:32 +0800

    refactor(app/error): modify validator error

--- src/Function/OnValidateError.ts
+++ src/Function/OnValidateError.ts
@@ -1,14 +1,18 @@
 import type { Context } from "hono";
 import type { ZodData } from "App/Decorator/Middleware.js";
 import isJson from "App/Function/IsJson.js";
+import ErrorBuilder from "App/Error/ErrorBuilder.js";
 
 export function onValidateError(c: Context<any, any, any>, data: ZodData<any>): Response {
-    const message = "Bad request performed.";
     const code = 400;
-    const propsOnServer = Object.keys(data);
+    const builder = new ErrorBuilder("Bad request.", "Bad request performed.");
+    builder
+        .setKind(data)
+        .setInstance(c.req.path)
+        .setType(`https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/${code}`);
 
     const jsonMethod = isJson(c.req.header());
-    if (jsonMethod) return c.json({ message, propsOnServer }, code);
+    if (jsonMethod) return c.json(builder.build(), code);
 
-    return c.text(message, code);
+    return c.text(builder.detail, code);
 }
